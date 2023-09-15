4fb1dc333549aa866d3ad5a75461f646d9c01210
XiaoZhiW <145045875+XiaoZhiW@users.noreply.github.com>
2023-09-15 12:56:48 +0800

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,118 @@
+﻿'use strict';
+
+(function () {
+
+    Office.onReady(function () {
+        // Office is ready.
+        $(document).ready(function () {
+            // The document is ready.
+            // Use this to check whether the API is supported in the Word client.
+            if (Office.context.requirements.isSetSupported('WordApi', '1.1')) {
+                // Do something that is only available via the new APIs.
+                $('#emerson').click(insertEmersonQuoteAtSelection);
+                $('#checkhov').click(insertChekhovQuoteAtTheBeginning);
+                $('#proverb').click(insertChineseProverbAtTheEnd);
+                $('#chinese').click(insertChineseAtTheEnd);
+                $('#supportedVersion').html('This code is using Word 2016 or later.');
+            } else {
+                // Lets you know that this code will not work with your version of Word.
+                $('#supportedVersion').html('This code requires Word 2016 or later.');
+            }
+        });
+    });
+
+    async function insertEmersonQuoteAtSelection() {
+        await Word.run(async (context) => {
+
+            // Create a proxy object for the document.
+            const thisDocument = context.document;
+
+            // Queue a command to get the current selection.
+            // Create a proxy range object for the selection.
+            const range = thisDocument.getSelection();
+
+            // Queue a command to replace the selected text.
+            range.insertText('"Hitch your wagon to a star." - Ralph Waldo Emerson\n', Word.InsertLocation.replace);
+
+            // Synchronize the document state by executing the queued commands,
+            // and return a promise to indicate task completion.
+            await context.sync();
+            console.log('Added a quote from Ralph Waldo Emerson.');
+        })
+            .catch(function (error) {
+                console.log('Error: ' + JSON.stringify(error));
+                if (error instanceof OfficeExtension.Error) {
+                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+                }
+            });
+    }
+
+    async function insertChekhovQuoteAtTheBeginning() {
+        await Word.run(async (context) => {
+
+            // Create a proxy object for the document body.
+            const body = context.document.body;
+
+            // Queue a command to insert text at the start of the document body.
+            body.insertText('"Knowledge is of no value unless you put it into practice." - Anton Chekhov\n', Word.InsertLocation.start);
+
+            // Synchronize the document state by executing the queued commands,
+            // and return a promise to indicate task completion.
+            await context.sync();
+            console.log('Added a quote from Anton Chekhov.');
+        })
+            .catch(function (error) {
+                console.log('Error: ' + JSON.stringify(error));
+                if (error instanceof OfficeExtension.Error) {
+                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+                }
+            });
+    }
+
+    async function insertChineseProverbAtTheEnd() {
+        await Word.run(async (context) => {
+
+            // Create a proxy object for the document body.
+            const body = context.document.body;
+
+            // Queue a command to insert text at the end of the document body.
+            body.insertText('"To know the road ahead, ask those coming back." - Chinese proverb\n', Word.InsertLocation.end);
+
+            // Synchronize the document state by executing the queued commands,
+            // and return a promise to indicate task completion.
+            await context.sync();
+            console.log('Added a quote from a Chinese proverb.');
+        })
+            .catch(function (error) {
+                console.log('Error: ' + JSON.stringify(error));
+                if (error instanceof OfficeExtension.Error) {
+                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+                }
+            });
+    }
+
+    async function insertChineseAtTheEnd() {
+        await Word.run(async (context) => {
+
+            // Create a proxy object for the document body.
+            const body = context.document.body;
+
+            // Queue a command to insert text at the end of the document body.
+            body.insertText('博观而约取\n', Word.InsertLocation.end);
+            body.insertText('厚积而薄发', Word.InsertLocation.end);
+            // Synchronize the document state by executing the queued commands,
+            // and return a promise to indicate task completion.
+            await context.sync();
+            console.log('Added a quote from a Chinese proverb.');
+        })
+            .catch(function (error) {
+                console.log('Error: ' + JSON.stringify(error));
+                if (error instanceof OfficeExtension.Error) {
+                    console.log('Debug info: ' + JSON.stringify(error.debugInfo));
+                }
+            });
+    }
+
+
+
+})();
